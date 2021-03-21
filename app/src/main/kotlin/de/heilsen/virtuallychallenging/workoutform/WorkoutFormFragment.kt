73e1fae4b6cf5c20aae80149339adadf5361f5e2
Sebastian Heil <sebastian.heil@1und1.de>
2021-03-21 21:26:34 +0100

    feat: Add workout via keyboard

--- app/src/main/kotlin/de/heilsen/virtuallychallenging/workoutform/WorkoutFormFragment.kt
+++ app/src/main/kotlin/de/heilsen/virtuallychallenging/workoutform/WorkoutFormFragment.kt
@@ -1,9 +1,12 @@
 package de.heilsen.virtuallychallenging.workoutform
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import androidx.core.widget.doAfterTextChanged
@@ -22,6 +25,7 @@ import java.time.Instant
 import java.time.LocalDate
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
+
 
 class WorkoutFormFragment : BottomSheetDialogFragment() {
 
@@ -59,19 +63,35 @@ class WorkoutFormFragment : BottomSheetDialogFragment() {
             datePicker.show(parentFragmentManager)
         }
         addButton.setOnClickListener {
-            val distance = distanceField.text.toString().toFloatOrNull()
-            if (distance == null) {
-                distanceField.requestFocus()
-                distanceContainer.error = "No distance provided"
-                return@setOnClickListener
-            }
-            val date = LocalDate.parse(dateField.text.toString(), DateTimeFormatter.ISO_LOCAL_DATE)
-            val instant = date.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant()
-            viewModel.dispatch(DashboardAction.AddWorkout(Workout(distance, instant)))
-            dismiss()
+            action(distanceField, distanceContainer, dateField)
         }
         distanceField.doAfterTextChanged { distanceContainer.isErrorEnabled = false }
         distanceField.doAfterFocus { distanceContainer.isErrorEnabled = false }
+
+        dateField.setOnEditorActionListener { textView, actionId, keyEvent ->
+            if (actionId != EditorInfo.IME_ACTION_DONE) return@setOnEditorActionListener false
+            val imm: InputMethodManager =
+                textView.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(textView.windowToken, 0)
+            action(distanceField, distanceContainer, dateField)
+            true
+        }
     }
 
+    private fun action(
+        distanceField: EditText,
+        distanceContainer: TextInputLayout,
+        dateField: EditText
+    ) {
+        val distance = distanceField.text.toString().toFloatOrNull()
+        if (distance == null) {
+            distanceField.requestFocus()
+            distanceContainer.error = getString(R.string.no_distance)
+            return
+        }
+        val date = LocalDate.parse(dateField.text.toString(), DateTimeFormatter.ISO_LOCAL_DATE)
+        val instant = date.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant()
+        viewModel.dispatch(DashboardAction.AddWorkout(Workout(distance, instant)))
+        dismiss()
+    }
 }
